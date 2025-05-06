39bf271a415b991d408a1e60e6e69aa9ce808d59
Sami-Khairy <samikhairy2003@gmail.com>
2025-05-06 15:29:11 +0100

    init project

---
+++ src/test/java/ma/khairy/imagegenerator/ImageGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.khairy.imagegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
